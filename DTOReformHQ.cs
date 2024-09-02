672f1e67f64d6412d15d378c0b597c52572f4c55
Jean Perez <69916732+motica23@users.noreply.github.com>
2024-09-02 10:00:37 -0400

    Se crea DTOReformHQ

---
+++ DTOReformHQ.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSDTO_v2019
+{
+    public class DTOReformHQ
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Aplicacion { get; set; }
+        [Required]
+        [StringLength(70)]
+        public string NombreArchivo { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Usuario { get; set; }
+        [Required]
+        [StringLength(60)]
+        public string NombreDocumento { get; set; }
+        public decimal Tasa { get; set; }
+        public int NumeroPaginas { get; set; }
+
+    }
+}
